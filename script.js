349564d7fc7b0f1bbbaf20ba381bd45760b23a26
Evie de HiCode <100520800+HiCode-Education@users.noreply.github.com>
2024-08-21 01:26:24 +0000

    Add script and style

---
+++ script.js
@@ -0,0 +1,9 @@
+/* No need to edit this file */
+
+function open_card() {
+  document.getElementById('outside').className = 'open-card';
+}
+
+function close_card() {
+  document.getElementById('outside').className = '';
+}
